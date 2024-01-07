26a35ce75bdddb5bb942b74d587edd953cc2fba1
Xi Huang <xhuang@ucdavis.edu>
2024-01-06 17:48:25 -0800

    Initial Commit

---
+++ AiryVibe/AiryVibeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiryVibeApp.swift
+//  AiryVibe
+//
+//  Created by 黄熙 on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct AiryVibeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
